cf7e2ef959cb5c10076759ef2eb6d11e42c0bb93
Hovander <217216X718010@na-idm.na-gad.nec.com>
2018-03-30 09:37:23 -0700

    Started Lesson 35

---
+++ Sec4_ObjectsAndFunctions/Lesson35.js
@@ -0,0 +1,14 @@
+//function statement
+greet();
+
+function greet(){
+    console.log('hi');
+}
+
+//anonymous function = variable name references function object, so ther is no need to name function
+anonymousGreet();
+
+var anonymousGreet = function(){
+    console.log('hi');
+};
+
